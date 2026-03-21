2f47a558fa2ca7cb81450f25db94e34a9957b228
Josh Hudson <josh@bamboonine.co.uk>
2026-03-21 09:25:32 +0000

    fix urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-const base = process.env.CI ? "/josh-hudson-thoughts" : "/";
+const base = process.env.CI ? "/josh-hudson-thoughts/" : "/";
 
 // https://astro.build/config
 export default defineConfig({
