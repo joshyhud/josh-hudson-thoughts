69c697b04219f134883023ad89927f37143570b9
Josh Hudson <josh@bamboonine.co.uk>
2026-03-25 08:11:55 +0000

    amended manifest and added a service worker for offline

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// public/sw.js
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open("v1").then((cache) => cache.addAll(["/", "/login"])));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
